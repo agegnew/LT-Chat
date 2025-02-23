70a237b33f9d945bb3db9d2b0e3de8c21060b602
agegnew <agegnew.mersha@gmail.com>
2025-02-23 07:33:52 -0800

    latest

--- lib/injection_container.dart
+++ lib/injection_container.dart
@@ -8,7 +8,10 @@ import 'features/auth/data/sources/auth_remote_data_source.dart';
 import 'features/auth/data/repositories/auth_repository_impl.dart';
 import 'features/auth/domain/repositories/auth_repository.dart';
 import 'features/auth/presentation/bloc/auth_bloc.dart';
-import 'features/chat/presentation/bloc/chat_bloc.dart';
+
+import 'features/chat/data/sources/chat_remote_data_source.dart';
+import 'features/chat/domain/repositories/chat_repository.dart';
+import 'features/chat/data/sources/chat_socket_data_source.dart';
 
 final sl = GetIt.instance;
 
@@ -23,16 +26,29 @@ Future<void> init() async {
         () => AuthRemoteDataSourceImpl(dio: sl<Dio>()),
   );
 
+  sl.registerLazySingleton<ChatRemoteDataSource>(
+        () => ChatRemoteDataSourceImpl(dio: sl<Dio>()),
+  );
+
+  sl.registerLazySingleton<ChatSocketDataSource>(
+        () => ChatSocketDataSource(),
+  );
+
   // 3. REPOSITORIES
   sl.registerLazySingleton<AuthRepository>(
         () => AuthRepositoryImpl(remoteDataSource: sl<AuthRemoteDataSource>()),
+  );
+
+  sl.registerLazySingleton<ChatRepository>(
+        () => ChatRepositoryImpl(remoteDataSource: sl<ChatRemoteDataSource>()),
   );
 
   // 4. BLOCS
   sl.registerFactory<AuthBloc>(
         () => AuthBloc(authRepository: sl<AuthRepository>()),
   );
+
   sl.registerFactory<ChatBloc>(
-        () => ChatBloc(chatRepository: sl<ChatRepositoryImpl>()),
+        () => ChatBloc(chatRepository: sl<ChatRepository>()),
   );
 }
