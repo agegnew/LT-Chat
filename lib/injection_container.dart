16f5219502718e1cb8fa2d1cd3d7ba97fa7f101f
agegnew <agegnew.mersha@gmail.com>
2025-02-23 07:19:12 -0800

    try to fix

--- lib/injection_container.dart
+++ lib/injection_container.dart
@@ -1,3 +1,5 @@
+import 'package:chat_frontend_clean_arch/features/chat/data/repositories/chat_repository_impl.dart';
+import 'package:chat_frontend_clean_arch/features/chat/presentation/bloc/chat_bloc.dart';
 import 'package:get_it/get_it.dart';
 import 'package:dio/dio.dart';
 import 'package:shared_preferences/shared_preferences.dart';
@@ -6,6 +8,7 @@ import 'features/auth/data/sources/auth_remote_data_source.dart';
 import 'features/auth/data/repositories/auth_repository_impl.dart';
 import 'features/auth/domain/repositories/auth_repository.dart';
 import 'features/auth/presentation/bloc/auth_bloc.dart';
+import 'features/chat/presentation/bloc/chat_bloc.dart';
 
 final sl = GetIt.instance;
 
@@ -28,5 +31,8 @@ Future<void> init() async {
   // 4. BLOCS
   sl.registerFactory<AuthBloc>(
         () => AuthBloc(authRepository: sl<AuthRepository>()),
+  );
+  sl.registerFactory<ChatBloc>(
+        () => ChatBloc(chatRepository: sl<ChatRepositoryImpl>()),
   );
 }
